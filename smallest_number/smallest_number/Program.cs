3e58f5fa6ad58b1ff48a9cb900ecb2ca006eb1fe
Yeamin-Sheikh <eaminshekh55@gmail.com>
2022-11-15 23:08:52 +0600

    Initial Commit

---
+++ smallest_number/smallest_number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace smallest_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("---FINDING THE SMALLEST NUMBER---");
+
+                int n1, n2, n3, smallest;
+                Console.Write("Enter the first number: ");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the second number: ");
+                n2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the thrid number: ");
+                n3 = Convert.ToInt32(Console.ReadLine());
+
+                smallest = n1 < n2 && n1 < n3 ? n1 : n2 < n3 ? n2 : n3;
+
+                Console.WriteLine("Among {0}, {1} and {2} the smallest number is {3}", n1, n2, n3, smallest);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter Only Numbers Please!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something Went Wrong!");
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
